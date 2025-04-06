6540b689e409c9f30a4e8287cc84f3ccaba7ab67
deeperName <491825969@qq.com>
2025-04-06 17:45:48 +0800

    合并

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import App from './App.vue'
 import router from './router'
 import TopBar from './layouts/TopBar.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 // import Antd from 'ant-design-vue';
 // import 'ant-design-vue/dist/reset.css';
 
 // createApp(App).use(router).mount('#app').component('Topbar',Topbar);
 const app=createApp(App) as any;
 app.component('TopBar',TopBar);
+
+// 使用 ElementPlus 插件
+app.use(ElementPlus);
 app.use(router);
 // app.use(Antd);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
